375b13ae090c075afa4a4e959c81e19dc56c12fa
Julien M <jmarchand@leadformance.com>
2013-08-26 19:13:50 +0200

    [YO] Initial commit

---
+++ app/index.js
@@ -0,0 +1,150 @@
+'use strict';
+var util = require('util');
+var path = require('path');
+var yeoman = require('yeoman-generator');
+
+var BridgeTemplateGenerator = module.exports = function BridgeTemplateGenerator(args, options, config) {
+  yeoman.generators.Base.apply(this, arguments);
+
+  this.on('end', function () {
+    this.installDependencies({ bower: false, npm: true, skipInstall: options['skip-install'] });
+  });
+
+  this.pkg = JSON.parse(this.readFileAsString(path.join(__dirname, '../package.json')));
+  // try to read .bridge-apikey.json if it exists
+  try {
+    this.existingCfg = JSON.parse(this.readFileAsString(path.join(this.destinationRoot(), '.bridge-apikey.json')));
+  } catch (e) {
+    // default values if config file doesn't exist
+    this.existingCfg = '';
+  }
+
+};
+
+util.inherits(BridgeTemplateGenerator, yeoman.generators.Base);
+
+BridgeTemplateGenerator.prototype.askFor = function askFor() {
+  var cb = this.async();
+
+  // have Yeoman greet the user.
+  console.log(this.yeoman);
+  console.log('This will download the latest Bridge starter template, and set-up Grunt tasks for testing and uploading template.\nYou will need an API key ("write_template") and a template slot if you want to upload your template with `grunt upload`.\n');
+
+  var templateTypes = [
+    {
+      name: "Desktop starter template",
+      value: "http://cl.ly/1c3N130Q3B1X/download/template.tar.gz"
+    },
+    {
+      name: "Mobile template",
+      value: "mobile-template-latest.zip"
+    },
+    {
+      name: "Facebook template",
+      value: "facebook-template-latest.zip"
+    },
+    {
+      name: "No template, I just want to set-up the Grunt tasks",
+      value: ""
+    }
+  ];
+
+  var serverUrls = [
+    {
+      name: "Integrator (.i)",
+      value: "https://api.i.leadformance.com"
+    },
+    {
+      name: "Internal QA (.q)",
+      value: "https://api.q.leadformance.com"
+    },
+    {
+      name: "Client QA (.c)",
+      value: "https://api.c.leadformance.com"
+    }
+  ];
+
+  var prompts = [
+/*    {
+      type: 'list',
+      name: 'templateType',
+      message: 'What kind of template do you want to use?',
+      default: 0,
+      choices: templateTypes
+    },
+*/    {
+      type: 'input',
+      name: 'apiKey',
+      message: 'Paste the "write_template" API key for that template:',
+      default: this.existingCfg.apiKey,
+      validate: function( value ){
+        if(value != '') {
+          return true;
+        } else {
+          return "Please enter the full API key";
+        }
+      }
+    },
+    {
+      type: 'list',
+      name: 'serverUrl',
+      message: 'On which server is it?',
+      default: this._.findIndex(serverUrls, { value: this.existingCfg.serverUrl }),
+      choices: serverUrls
+    },
+    {
+      type: 'input',
+      name: 'templateSlot',
+      message: 'What is the template slot ID?',
+      default: this.existingCfg.templateSlot,
+      validate: function( value ) {
+        var valid = !isNaN(parseFloat(value));
+        return valid || "Please enter a number";
+      },
+      filter: Number
+    },
+  ];
+
+  this.prompt(prompts, function (props) {
+    this.apiKey = props.apiKey;
+    this.serverUrl = props.serverUrl;
+    this.templateSlot = props.templateSlot;
+    this.templateType = props.templateType;
+
+    cb();
+  }.bind(this));
+};
+
+BridgeTemplateGenerator.prototype.app = function app() {
+  this.copy('package.json', 'package.json');
+  this.copy('Gruntfile.js', 'Gruntfile.js');
+  this.copy('gitignore', '.gitignore');
+};
+
+BridgeTemplateGenerator.prototype.projectfiles = function projectfiles() {
+  this.template('_bridge-apikey.json', '.bridge-apikey.json');
+};
+
+BridgeTemplateGenerator.prototype.fetchTemplate = function fetchTemplate() {
+  var cb = this.async();
+  var destinationRoot = this.destinationRoot();
+
+  if (this.templateType != '') {
+    console.log("Downloading " + this.templateType + " in path: " + destinationRoot);
+//    this.tarball(this.templateType, path.join(destinationRoot, 'temp'), function (err) {
+      this.bowerInstall(this.templateType, {}, function (err) {
+      if (err) {
+        return cb(err);
+      }
+      cb();
+    });
+  }
+
+/*  this.remote('julienma', 'Bridget', function (err, remote) {
+    if (err) {
+      return cb(err);
+    }
+    remote.directory('build', path.join(destinationRoot, 'build'));
+    cb();
+  });*/
+};
---
+++ app/templates/Gruntfile.js
@@ -0,0 +1,356 @@
+/*global module:false*/
+module.exports = function(grunt) {
+
+  // Helpers
+  function shellCheckCallback(err, stdout, stderr, callback) {
+    // API should output a string containing the template id after successful upload
+    if (stdout.indexOf('Status: 200') == -1) {
+      grunt.log.error();
+      grunt.warn("Can't upload template, check your .bridge-apikey.json");
+    } else {
+      console.log(stdout);
+      grunt.log.ok();
+    }
+    callback();
+  }
+
+  function shellUploadCallback(err, stdout, stderr, callback) {
+    // API should output a string containing the template id after successful upload
+    if (stdout.indexOf('{"id":') == -1) {
+      grunt.fatal("Can't upload template, check your .bridge-apikey.json");
+    } else {
+      console.log(stdout);
+      grunt.log.ok();
+    }
+    callback();
+  }
+
+  // Project configuration.
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    config: grunt.file.readJSON('.bridge-apikey.json'),
+    banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' +
+      '<%= grunt.template.today("yyyy-mm-dd") %> */\n',
+    dirs: {
+      js:     'javascripts',
+      css:    'stylesheets',
+      img:    'images',
+      output: '_output',
+      build:  '_build',
+      temp:   '<%= dirs.output %>/_temp'
+    },
+
+    // Task configuration.
+
+    /* Lint JS */
+    jshint: {
+      options: {
+        curly:      true,
+        immed:      true,
+        latedef:    true,
+        newcap:     true,
+        noarg:      true,
+        sub:        true,
+        undef:      false,
+        unused:     true,
+        boss:       true,
+        eqnull:     true,
+        browser:    true,
+        globals: {
+          jQuery:   true,
+          console:  true,
+          module:   true,
+          document: true
+        }
+      },
+      gruntfile: {
+        src: 'Gruntfile.js'
+      },
+      all: {
+        // do not lint external resources (like bootstrap / jquery), too messy
+        src: ['<%= dirs.js %>/**/*.js', '!**/bootstrap.js', '!**/jquery.*.js']
+      }
+    },
+    /* Lint CSS */
+    csslint: {
+      all: {
+        options: {
+          /* This set of options is only made to catch potentially harmful errors. Writing beautiful CSS is still up to you. */
+          'important': false,
+          'adjoining-classes': false,
+          // 'known-properties': false,
+          // 'box-sizing': false,
+          'box-model': false,
+          // 'overqualified-elements': false,
+          // 'display-property-grouping': false,
+          // 'bulletproof-font-face': false,
+          // 'compatible-vendor-prefixes': false,
+          // 'regex-selectors': false,
+          // 'errors': false,
+          'duplicate-background-images': false,
+          // 'duplicate-properties': false,
+          // 'empty-rules': false,
+          // 'selector-max-approaching': false,
+          // 'gradients': false,
+          // 'fallback-colors': false,
+          'font-sizes': false,
+          // 'font-faces': false,
+          'floats': false,
+          // 'star-property-hack': false,
+          'outline-none': false,
+          // 'import': false,
+          'ids': false,
+          // 'underscore-property-hack': false,
+          // 'rules-count': false,
+          'qualified-headings': false,
+          // 'selector-max': false,
+          // 'shorthand': false,
+          // 'text-indent': false,
+          'unique-headings': false,
+          'universal-selector': false
+          // 'unqualified-attributes': false,
+          // 'vendor-prefix': false,
+          // 'zero-units': false
+        },
+        src: ['<%= dirs.css %>/**/*.css', '!**/bootstrap.css']
+      }
+    },
+    /* Concat JS */
+    concat: {
+      options: {
+        banner: '<%= banner %>',
+        stripBanners: true,
+        separator: ';'
+      },
+      build: {
+        src: ['<%= dirs.js %>/**/*.js'],
+        dest: '<%= dirs.temp %>/<%= dirs.js %>/combined.js'
+      }
+    },
+    /* Minify JS */
+    uglify: {
+      options: {
+        banner: '<%= banner %>'
+      },
+      build: {
+        src:  '<%= concat.build.dest %>',
+        dest: '<%= dirs.output %>/<%= dirs.js %>/combined.min.js'
+      }
+    },
+    /* Concat & Minify CSS */
+    cssmin: {
+      add_banner: {
+        options: {
+          banner: '<%= banner %>'
+        },
+        files: {
+          '<%= dirs.output %>/<%= dirs.css %>/combined.min.css': ['<%= dirs.css %>/**/*.css']
+        }
+      }
+    },
+    /* Optimize images */
+    imagemin: {
+      build: {
+        options: {
+          optimizationLevel: 3,
+          progressive: true
+        },
+        files: [{
+          expand: true,
+          cwd:  '<%= dirs.img %>',
+          src:  '**/*.{png,jpg,jpeg}',
+          dest: '<%= dirs.output %>/<%= dirs.img %>'
+        }]
+      }
+    },
+    /* Clean dirs */
+    clean: {
+      temp:   ["<%= dirs.temp %>"],
+      output: ["<%= dirs.output %>"],
+      build:  ["<%= dirs.build %>"]
+    },
+    /* Copy other files to output directory */
+    copy: {
+      build: {
+        files: [{
+          expand: true,
+          // exclude (npm/grunt/build/js/css/images) files
+          src: ['**',
+                '!node_modules/**',
+                '!.bridge-apikey.json',
+                '!grunt-README.md',
+                '!Gruntfile.js',
+                '!package.json',
+                '!<%= dirs.output %>/**',
+                '!<%= dirs.js %>/**',
+                '!<%= dirs.css %>/**',
+                '!<%= dirs.img %>/**'],
+          dest: '<%= dirs.output %>'
+        }]
+      }
+    },
+    /* Rev assets */
+    /* Do not use rev + usemin for now, as of now, Bridge's CSS & JS are not compatible. Would need some CSS preprocessor to input image names in variables, and some time to clean JS... */
+    // rev: {
+    //   assets: {
+    //     files: [{
+    //       src: [
+    //         '<%= dirs.output %>/<%= dirs.js %>/**/*.js',
+    //         '<%= dirs.output %>/<%= dirs.css %>/**/*.css',
+    //         '<%= dirs.output %>/<%= dirs.img %>/**/*.{jpg,jpeg,gif,png}',
+    //         '<%= dirs.output %>/fonts/**/*.{eot,svg,ttf,woff}'
+    //       ]
+    //     }]
+    //   }
+    // },
+    // usemin: {
+    //     html: ['<%= dirs.output %>/**/*.html'],
+    //     css: ['<%= dirs.output %>/<%= dirs.css %>/**/*.css'],
+    //     options: {
+    //         dirs: ['<%= dirs.output %>']
+    //     }
+    // },
+    /* Zip */
+    compress: {
+      build: {
+        options: {
+          archive: '<%= dirs.build %>/_template.zip',
+          level: 9,
+          pretty: true
+        },
+        files: [
+          {expand: true, cwd: '<%= dirs.output %>/', src: ['**'], dest: ''}
+        ]
+      }
+    },
+    /* Open Finder/Explorer in the .zip folder */
+    open : {
+      build : {
+        path : '<%= dirs.build %>/'
+      }
+    },
+    /* Upload with cURL */
+    shell: {
+      check: {
+        command: 'curl --head "<%= config.serverUrl %>/templates/<%= config.templateSlot %>.json?oauth_token=<%= config.apiKey %>"',
+        options: {
+          callback: shellCheckCallback
+        }
+      },
+      upload: {
+        command: 'curl -F "template=@<%= dirs.build %>/_template.zip" -F "_method=PUT" "<%= config.serverUrl %>/templates/<%= config.templateSlot %>.json?oauth_token=<%= config.apiKey %>"',
+        options: {
+          callback: shellUploadCallback
+        }
+      }
+    },
+    /* Watch */
+    watch: {
+      options: {
+        livereload: true
+      },
+      gruntfile: {
+        files: '<%= jshint.gruntfile.src %>',
+        tasks: ['jshint:gruntfile']
+      },
+      js: {
+        files: ['<%= jshint.all.src %>'],
+        tasks: ['jshint:all']
+      },
+      css: {
+        files: ['<%= csslint.all.src %>'],
+        tasks: ['csslint:all']
+      }
+    },
+    /* Notify Hooks */
+    notify_hooks: {
+      options: {
+        enabled: true,
+        max_jshint_notifications: 5, // maximum number of notifications from jshint output
+        title: "Bridge Grunt"
+      }
+    },
+    /* Notify specific messages */
+    notify: {
+      build: {
+        options: {
+          title: "Bridge Grunt BUILD",
+          message: "HOY, template zip'd!"
+        }
+      },
+      upload: {
+        options: {
+          title: "Bridge Grunt UPLOAD",
+          message: "YAY, template's been uploaded (slot #<%= config.templateSlot %>)"
+        }
+      }
+    }
+  });
+
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-csslint');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-compress');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-open');
+  grunt.loadNpmTasks('grunt-shell');
+  grunt.loadNpmTasks('grunt-notify');
+  // grunt.loadNpmTasks('grunt-rev');
+  // grunt.loadNpmTasks('grunt-usemin');
+
+  // This is required if you use any options.
+  grunt.task.run('notify_hooks');
+
+  // Register tasks.
+
+  /* Dev: test */
+  grunt.registerTask('check', [
+    'jshint',
+    'csslint'
+  ]);
+  /* Prod: build .zip */
+  grunt.registerTask('build', [
+    'jshint',
+    'csslint',
+    'clean',
+    'concat',
+    'uglify',
+    'clean:temp',
+    'cssmin',
+    'imagemin',
+    'copy',
+    'compress',
+    'clean:output',
+    'open:build',
+    'notify:build'
+  ]);
+  grunt.registerTask('upload', [
+    'jshint',
+    'csslint',
+    'clean',
+    'concat',
+    'uglify',
+    'clean:temp',
+    'cssmin',
+    'imagemin',
+    'copy',
+    'compress',
+    'clean:output',
+    'shell:check',
+    'shell:upload',
+    'clean:build',
+    'notify:upload'
+  ]);
+  grunt.registerTask('server', ['watch']);
+
+  // Default task.
+  grunt.registerTask('default', 'check');
+
+};
